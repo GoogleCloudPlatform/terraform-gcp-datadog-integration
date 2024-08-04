3f8d5adbff848e5b4f07a74d18037d8f71e5c0eb
diegonz2 <46681214+diegonz2@users.noreply.github.com>
2024-08-04 05:38:15 -0300

    Initial commit
    Files needed for the integration

---
+++ gcp-datadog-module/dataflow-job.tf
@@ -0,0 +1,38 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#####################################################################
+# CREATE A DATAFLOW JOB THAT USES THE 'PUB/SUB TO DATADOG' TEMPLATE #
+#####################################################################
+
+resource "google_dataflow_job" "pubsub_stream_to_datadog" {
+  name                    = var.dataflow_job_name
+  template_gcs_path       = "gs://dataflow-templates-${var.region}/latest/Cloud_PubSub_to_Datadog"
+  temp_gcs_location       = "gs://${google_storage_bucket.temp_files_bucket.id}/tmp_dir"
+  region                  = var.region
+  service_account_email   = google_service_account.dataflow_datadog_export_sa.email
+  network                 = data.google_compute_network.vpc.name
+  subnetwork              = data.google_compute_subnetwork.dataflow_subnetwork.self_link
+  ip_configuration        = "WORKER_IP_PRIVATE"
+  max_workers             = 3
+  enable_streaming_engine = true
+  parameters = {
+    inputSubscription     = google_pubsub_subscription.datadog_topic_sub.id,
+    url                   = var.datadog_site_url,
+    apiKeySecretId        = google_secret_manager_secret_version.secret_version.name,
+    apiKeySource          = "SECRET_MANAGER",
+    outputDeadletterTopic = google_pubsub_topic.output_dead_letter.id
+  }
+  on_delete = "cancel"
+}
---
+++ gcp-datadog-module/variables.tf
@@ -0,0 +1,86 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project."
+}
+
+variable "region" {
+  type        = string
+  description = "The GCP region where resources will be deployed, this must be the same region where the subnet exists."
+}
+
+variable "dataflow_job_name" {
+  type        = string
+  description = "Dataflow job name"
+  default     = "datadog-export-job"
+}
+
+variable "dataflow_temp_bucket_name" {
+  type        = string
+  description = "GCS Bucket to write Dataflow temporary files. Must start and end with letter or number. Must be between 3 and 63 characters."
+  default     = "temp-files-dataflow-bucket-"
+}
+
+variable "topic_name" {
+  type        = string
+  description = "Name of the Pub/Sub Topic to receive logs from Google Cloud."
+  default     = "datadog-export-topic"
+}
+
+variable "subscription_name" {
+  type        = string
+  description = "Name of the Pub/Sub subscription to receive logs from Google Cloud."
+  default     = "datadog-export-sub"
+}
+
+variable "vpc_name" {
+  type        = string
+  description = "Name of the VPC used for Dataflow Virtual Machines."
+}
+
+variable "subnet_name" {
+  type        = string
+  description = "Name of the subnets used for Dataflow Virtual Machines."
+}
+
+variable "datadog_api_key" {
+  type        = string
+  description = "Datadog API Key for integration."
+  sensitive   = true
+}
+
+variable "datadog_site_url" {
+  type        = string
+  description = "Datadog Logs API URL, it will depends on the Datadog site region (https://docs.datadoghq.com/integrations/google_cloud_platform/#4-create-and-run-the-dataflow-job)."
+}
+
+variable "log_sink_in_folder" {
+  type        = bool
+  description = "Set to true if the Log Sink should be created at the folder level."
+  default     = false
+}
+
+variable "folder_id" {
+  type        = string
+  description = "Folder ID where the Log Sink should be created. Set to null if not in a folder."
+  default     = ""
+}
+
+variable "inclusion_filter" {
+  type        = string
+  description = "Inclusion filter to be used by the Log sink for the logs to be forwarded to Datadog."
+  default     = ""
+}
