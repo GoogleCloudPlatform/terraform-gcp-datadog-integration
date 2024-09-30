5d909cf5cda7a55588f6b60a4cc7bfa5d5fdbecd
diegonz2 <46681214+diegonz2@users.noreply.github.com>
2024-09-30 00:02:25 -0300

    Comment about using the region of existing subnet

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@
 module "datadog-integration" {
   source                    = "./gcp-datadog-module"
   project_id                = ""
-  region                    = ""
   dataflow_job_name         = ""
   dataflow_temp_bucket_name = ""
   topic_name                = "datadog-export-topic"
   subscription_name         = "datadog-export-sub"
   vpc_name                  = ""
   subnet_name               = ""
+  region                    = ""   # Specify the region of your existing subnet.
   datadog_api_key           = ""
   datadog_site_url          = ""
   log_sink_in_folder        = true
